cb237bb5ef995afc4429cdd709ccd5e215ff4457
kelemul <kelemu3509@gmail.com>
2019-12-03 17:17:31 +0300

    Initial commit

---
+++ ConsoleApp1/Calc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Another_1
+    {
+      public dynamic Add(int x,int y)
+        {
+            var sum = x + y;
+            return sum;
+        }
+
+        public dynamic  Div(float x, float y)
+        {
+            var div = x / y;
+            return div;
+        }
+        public dynamic  Diff(int x, int y)
+        {
+            var min = x - y;
+            return min;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            var inpput1 = 20;
+            var input2 = 30;
+            var input3 = 40;
+
+            
+            
+            Console.WriteLine("enough for today");
+            Another_1 a = new Another_1();
+            var sum = a.Add(inpput1, input2);
+            var diff = a.Diff(sum, input3);
+            float div = a.Div(input2, input3);
+            Console.WriteLine("The Sum of  " + inpput1 + " and " + input2 + " is = " + sum);
+            Console.WriteLine("The difference between " + sum + " and " + input3 + " is = " + diff);
+            Console.WriteLine("The division of " + input2 + " and " + input3 + " is = " + div);
+            Console.WriteLine("====================  THIS IS THE END =====================");
+
+            int ii= Console.Read();
+          
+            if (ii <= 50)
+            {
+                Console.WriteLine(ii+" is less than 50");
+            }
+            else
+            {
+                Console.Write(ii + " is greater than 50");
+            }
+           
+
+
+        }
+    }
+}
